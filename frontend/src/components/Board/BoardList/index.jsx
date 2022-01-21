8db4255bd18aacd76e023b1bfa5acc39be8c7e19
황소현 <t01081418141@naver.com>
2022-01-21 13:56:46 +0900

    🔧Add BoardList

---
+++ frontend/src/components/Board/BoardList/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useContext } from 'react';
+import Wrapper from '../../Auth/SignResponsiveDialog/styles';
+
+import listData from '../dump.json';
+
+const BoardList=props=>{
+
+    return(
+        <Wrapper className='root'>
+            <h2>BoardList</h2>
+        </Wrapper>
+    )
+}
+
+export default BoardList;
--- frontend/src/pages/Community/index.jsx
+++ frontend/src/pages/Community/index.jsx
@@ -1,11 +1,14 @@
 import React, { useContext, useEffect } from 'react';
 
+import BoardList from '../../components/Board/BoardList/';
 import Layout from '../../layout/';
 
 const Community = () => {
   return (
     <Layout>
         <h2> 커뮤니티 페이지 </h2>
+        <br/>
+        <BoardList/>
     </Layout>);
 };
 
