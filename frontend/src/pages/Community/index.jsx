2d73ca11beecf145f24f535fa9f763bec88d0ba5
황소현 <t01081418141@naver.com>
2022-01-24 14:37:37 +0900

    🔧 Add pagination

--- frontend/src/pages/Community/index.jsx
+++ frontend/src/pages/Community/index.jsx
@@ -1,29 +1,58 @@
-import { Grid, Button } from '@material-ui/core';
+// import { Grid, Button } from '@material-ui/core';
 import React, { useContext, useEffect } from 'react';
+
+import {Grid,Button,Pagination} from '@mui/material';
 
 import BoardList from '../../components/Board/BoardList/';
 import { useHistory } from 'react-router-dom';
 import SearchComponent from '../../components/Search/SearchComponent';
 import Layout from '../../layout/';
 
+import { useLocalStorageSetState } from '../../common/CommonHooks';
+import { CommonContext } from '../../context/CommonContext';
+import { ViewContext } from '../../context/ViewContext';
 import Wrapper from './styles';
 
 const Community = () => {
+  const {
+    user
+  } = useContext(CommonContext);
+
   let history = useHistory();
 
-  const onClickCommunityWriteHandler=()=>{
-    history.push('/CommunityWrite');
-  }
+  const [searchValue, setSearchValue] = useLocalStorageSetState('', 'search');
+  const [page, setPage] = React.useState(1);
   
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
+  const onClickCommunityWriteHandler = () => {
+    history.push('/CommunityWrite');
+  };
+
   return (
-    <Layout>  
-      <Wrapper>
-        <Grid>
-          <Button variant="primary" onClick={onClickCommunityWriteHandler}>글쓰기</Button>
-        </Grid>
-        <BoardList />
-      </Wrapper>
-    </Layout>
+    <ViewContext.Provider
+      value={{
+        searchValue,
+        setSearchValue,
+      }}
+    >
+      <Layout>
+        <Wrapper>
+          <Grid>
+            {user.status && <Button variant="primary" onClick={onClickCommunityWriteHandler}>
+              글쓰기
+            </Button>}
+            <SearchComponent />
+          </Grid>
+            <BoardList page={page} />
+          <Grid>
+            <Pagination count={10} page={page} onChange={handlePageChange} />
+          </Grid>
+        </Wrapper>
+      </Layout>
+    </ViewContext.Provider>
   );
 };
 
