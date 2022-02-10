b3572502eae4257be320a485b268c5f33bf12620
sohyeon <t01081418141@naver.com>
2022-02-10 18:15:13 +0900

    🐛 Fix myfarm enter error

--- frontend/src/pages/MyFarm/index.jsx
+++ frontend/src/pages/MyFarm/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 import Layout from '../../layout/';
 import Wrapper from './styles';
@@ -7,9 +8,26 @@ import FarmInfo from './FarmInfo';
 import FarmStatus from './FarmStatus';
 import FarmHistory from './FarmHistory';
 
+import Swal from 'sweetalert2';
+
 const MyFarm = () => {
   // const user = useSelector(state => state.Auth.user);
 
+  const farm = useSelector(state => state.Farm.farm);
+  let history = useHistory();
+
+  console.log(farm);
+  if(!farm)
+  {
+    Swal.fire({
+      icon: 'warning',
+      title: '농장 데이터가 없습니다',
+      text: '내 농장 서비스를 이용하시려면 IoT 기기를 신청해주세요',
+    });
+    history.push('./')
+  }
+
+  if(!farm) return(<></>)
   return (
     <Layout>
       <Wrapper>
