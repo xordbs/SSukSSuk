4b4fca02e6491191d990ead8e1b8dab4020aefe4
LDJ <ehdwnsdl1210@gmail.com>
2022-02-10 18:19:18 +0900

    🔨 Fix MyFarmHistory Function

--- frontend/src/components/Board/MyFarmBoardList/index.jsx
+++ frontend/src/components/Board/MyFarmBoardList/index.jsx
@@ -125,7 +125,6 @@ function EnhancedTableHead(props) {
 EnhancedTableHead.propTypes = {
   numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
   orderBy: PropTypes.string.isRequired,
   rowCount: PropTypes.number.isRequired,
