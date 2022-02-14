6ac635a028afa96ed1ef554708aa18890bce1dfc
sohyeon <t01081418141@naver.com>
2022-02-14 13:03:39 +0900

    🔨 Add iot regi db connection

--- frontend/src/pages/MyFarm/index.jsx
+++ frontend/src/pages/MyFarm/index.jsx
@@ -1,13 +1,9 @@
 import React, { useContext, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import { Box } from '@mui/material';
 
 import { CommonContext } from '../../context/CommonContext';
-import { ViewContext } from '../../context/ViewContext';
-
-import { Dialog, useMediaQuery } from '@material-ui/core';
 
 import Layout from '../../layout/';
 import Wrapper from './styles';
@@ -16,8 +12,6 @@ import FarmStatus from './FarmStatus';
 import FarmHistory from './FarmHistory';
 
 import RegiIot from '../../components/RegiIoT';
-
-import Swal from 'sweetalert2';
 
 const Background = styled(Box)({
   position: 'absolute',
@@ -31,34 +25,24 @@ const Background = styled(Box)({
 });
 
 const MyFarm = () => {
-  // const user = useSelector(state => state.Auth.user);
   const { setRegiIotDialogOpen } = useContext(CommonContext);
 
   const farm = useSelector(state => state.Farm.farm);
-  let history = useHistory();
 
-  // useEffect(() => {
-  //   if (!farm) {
-  //     // 농장 iot 신청 여부 조사
+  useEffect(() => {
+    // 농장 iot 신청 여부 조사
+    if (!farm) {
+      // 신청 안했으면
+      setRegiIotDialogOpen(true);
+    }
+  });
 
-  //     // 신청 했으면
-  //     // Swal.fire({
-  //     //   title: '기기등록이 되지 않았습니다',
-  //     //   text: 'IoT에서 농장등록을 먼저 해주세요',
-  //     // });
-  //     // history.push('./')
-
-  //     // 신청 안했으면
-  //     setRegiIotDialogOpen(true);
-  //   }
-  // });
-
-  // if (!farm)
-  //   return (
-  //     <Layout>
-  //       <RegiIot />
-  //     </Layout>
-  //   );
+  if (!farm)
+    return (
+      <Layout>
+        <RegiIot />
+      </Layout>
+    );
   return (
     <Layout>
       <Background
@@ -68,7 +52,7 @@ const MyFarm = () => {
         }}
       />
       <Wrapper>
-        {/* <h2> 내 농장 페이지 </h2> */}
+        <h1> 내 농장 페이지 </h1>
         <FarmInfo />
         <FarmStatus />
         <FarmHistory />
